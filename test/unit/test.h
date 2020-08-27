ed76fa4a7e9ef7c4803f4c22a0246a348a7095c9
Tobias Nießen <tniessen@tnie.de>
2020-08-27 17:59:45 +0200

    test: use ctest for timeout, check python version

--- test/unit/test.h
+++ test/unit/test.h
@@ -3,20 +3,9 @@
 #endif
 
 #include <assert.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define TEST_TIMEOUT 10
 
 #define TEST_MAIN(fn)                                                          \
-  void on_test_timeout(__attribute__((unused)) int sig) {                      \
-    abort();                                                                   \
-  }                                                                            \
-                                                                               \
   int main(void) {                                                             \
-    signal(SIGALRM, on_test_timeout);                                          \
-    alarm(TEST_TIMEOUT);                                                       \
     fn();                                                                      \
     return 0;                                                                  \
   }
