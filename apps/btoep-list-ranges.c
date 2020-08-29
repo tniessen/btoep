2d5ff5fb3a33adb0c6dc2d591dec55bcc1561e08
Tobias Nießen <tniessen@tnie.de>
2020-08-29 23:09:55 +0200

    apps,test: add '--range-format' to list-ranges

--- apps/btoep-list-ranges.c
+++ apps/btoep-list-ranges.c
@@ -6,11 +6,20 @@
 #include "util/common.h"
 #include "util/opt.h"
 
-static inline void prange(uint64_t start, uint64_t end) {
-  printf("%" PRIu64 "..%" PRIu64 "\n", start, end - 1);
+typedef void (*print_range_fn)(btoep_range range);
+typedef bool (*list_fn)(btoep_dataset* dataset, print_range_fn print_range);
+
+static void print_range_excl(btoep_range range) {
+  printf("%" PRIu64 "...%" PRIu64 "\n",
+         range.offset, range.offset + range.length);
 }
 
-static bool list_data_ranges(btoep_dataset* dataset) {
+static void print_range_incl(btoep_range range) {
+  printf("%" PRIu64 "..%" PRIu64 "\n",
+         range.offset, range.offset + range.length - 1);
+}
+
+static bool list_data_ranges(btoep_dataset* dataset, print_range_fn print_range) {
   uint64_t total_size;
   if (!btoep_data_get_size(dataset, &total_size))
     return false;
@@ -19,13 +28,13 @@ static bool list_data_ranges(btoep_dataset* dataset) {
     btoep_range range;
     if (!btoep_index_iterator_next(dataset, &range))
       return false;
-    prange(range.offset, range.offset + range.length);
+    print_range(range);
   }
 
   return true;
 }
 
-static bool list_missing_ranges(btoep_dataset* dataset) {
+static bool list_missing_ranges(btoep_dataset* dataset, print_range_fn print_range) {
   uint64_t total_size;
   if (!btoep_data_get_size(dataset, &total_size))
     return false;
@@ -39,7 +48,7 @@ static bool list_missing_ranges(btoep_dataset* dataset) {
 
     if (range.offset != 0) {
       assert(range.offset > prev_end_offset);
-      prange(prev_end_offset, range.offset);
+      print_range(btoep_mkrange(prev_end_offset, range.offset - prev_end_offset));
     }
 
     prev_end_offset = range.offset + range.length;
@@ -47,7 +56,7 @@ static bool list_missing_ranges(btoep_dataset* dataset) {
 
   // TODO: What if prev_end_offset > total_size ? Fail ?
   if (prev_end_offset != total_size) {
-    prange(prev_end_offset, total_size);
+    print_range(btoep_mkrange(prev_end_offset, total_size - prev_end_offset));
   }
 
   return true;
@@ -55,11 +64,31 @@ static bool list_missing_ranges(btoep_dataset* dataset) {
 
 typedef struct {
   dataset_path_opts paths;
+  print_range_fn range_format;
   bool missing;
 } cmd_opts;
 
+static bool opt_accept_range_format(void* out, const char* value) {
+  cmd_opts* result = out;
+  if (result->range_format != NULL)
+    return false;
+  if (strcmp(value, "exclusive") == 0) {
+    result->range_format = print_range_excl;
+  } else if (strcmp(value, "inclusive") == 0) {
+    result->range_format = print_range_incl;
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
 int main(int argc, char** argv) {
-  opt_def options[4] = {
+  opt_def options[5] = {
+    {
+      .name = "--range-format",
+      .accept = opt_accept_range_format
+    },
     {
       .name = "--missing",
       .accept = opt_accept_bool_flag_once,
@@ -67,12 +96,13 @@ int main(int argc, char** argv) {
     }
   };
 
-  opt_add_nested(options + 1, dataset_path_opt_defs, 3, offsetof(cmd_opts, paths));
+  opt_add_nested(options + 2, dataset_path_opt_defs, 3, offsetof(cmd_opts, paths));
 
   cmd_opts opts = {
-    .missing = false
+    .missing = false,
+    .range_format = NULL
   };
-  if (!opt_parse(options, 4, &opts, (size_t) argc - 1, argv + 1)) {
+  if (!opt_parse(options, 5, &opts, (size_t) argc - 1, argv + 1)) {
     fprintf(stderr, "error\n");
     return B_EXIT_CODE_USAGE_ERROR;
   }
@@ -88,11 +118,13 @@ int main(int argc, char** argv) {
     return B_EXIT_CODE_APP_ERROR;
   }
 
-  bool (*list_ranges)(btoep_dataset* dataset);
-  list_ranges = opts.missing ? list_missing_ranges : list_data_ranges;
+  list_fn list_ranges = opts.missing ? list_missing_ranges : list_data_ranges;
+  print_range_fn print_range = opts.range_format;
+  if (print_range == NULL)
+    print_range = print_range_incl;
 
   if (!btoep_index_iterator_start(&dataset) ||
-      !list_ranges(&dataset)) {
+      !list_ranges(&dataset, print_range)) {
     const char* message;
     btoep_get_error(&dataset, NULL, &message);
     fprintf(stderr, "Error: %s\n", message);
--- test/system/test-list-ranges.py
+++ test/system/test-list-ranges.py
@@ -3,10 +3,12 @@ import unittest
 
 class ListRangesTest(SystemTest):
 
-  def cmdListRanges(self, dataset, missing = False):
+  def cmdListRanges(self, dataset, missing = False, format = None):
     args = ['btoep-list-ranges', '--dataset', dataset]
     if missing:
       args.append('--missing')
+    if format is not None:
+      args.append('--range-format=' + format)
     result = self.cmd(*args, text=True)
     self.assertEqual(result.stderr, '')
     return result.stdout
@@ -32,6 +34,12 @@ class ListRangesTest(SystemTest):
     self.assertEqual(ranges, '0..0\n')
     ranges = self.cmdListRanges(dataset, True)
     self.assertEqual(ranges, '1..524287\n')
+
+    # Test the same dataset again, but using a different format
+    ranges = self.cmdListRanges(dataset, format='exclusive')
+    self.assertEqual(ranges, '0...1\n')
+    ranges = self.cmdListRanges(dataset, missing=True, format='exclusive')
+    self.assertEqual(ranges, '1...524288\n')
 
     # Test a 512 KiB dataset with another non-empty index
     dataset = self.createDataset(b'\x00' * 1024 * 512, b'\x81\x01\x7f\x00\x7f')
